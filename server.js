34198ff115cec32cb28c7a1eb75076905e949ef3
AbdelrahmanAbbasN2 <bob.krons123@gmail.com>
2025-12-07 16:46:33 +0200

    Adding new updates

--- server.js
+++ server.js
@@ -28,4 +28,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Salary Calculation API");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
+
+app.listen(PORT, () => console.log("Server running on port 3000"));
